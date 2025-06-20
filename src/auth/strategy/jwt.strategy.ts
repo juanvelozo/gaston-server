1f10f4d13f7b31d5c8cbde6977824ec95c821dcc
juanvelozo_ <juanvelozomoreno18@gmail.com>
2025-06-20 03:30:05 -0300

    actualiza comentarios

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,23 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { JwtPayload } from '../decorator/get-user.decorator'; // o desde donde la tengas exportada
+import { JwtPayload } from '../decorator/get-user.decorator';
 
+/**
+ * Estrategia para manejar la autenticación JWT.
+ * Utiliza la estrategia de Passport para validar tokens JWT.
+ */
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  /**
+   * Constructor de JwtStrategy.
+   * @param config - Servicio de configuración para obtener variables de entorno.
+   * @throws Error si JWT_SECRET no está definido en las variables de entorno.
+   */
   constructor(config: ConfigService) {
+    // Obtiene el secreto de JWT desde las variables de entorno.
     const jwtSecret = config.get<string>('JWT_SECRET');
 
+    // Verifica si JWT_SECRET está definido.
     if (!jwtSecret) {
       throw new Error('JWT_SECRET no está definido en las variables de entorno');
     }
 
+    // Configura la estrategia de Passport para extraer el token desde el encabezado de autorización.
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: jwtSecret,
     });
   }
 
+  /**
+   * Valida el payload del JWT.
+   * @param payload - Contenido del JWT.
+   * @returns El payload, que será accesible desde req.user.
+   */
   validate(payload: JwtPayload): JwtPayload {
     return payload; // será accesible desde req.user
   }
