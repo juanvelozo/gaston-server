e8ad2075aaf7bc49cb6178f6f209301d00f2768e
juanvelozo_ <juanvelozomoreno18@gmail.com>
2025-06-23 23:02:11 -0300

    [USER] agrega mensaje de error en cambiar contraseña

--- src/user/dto/change-password.dto.ts
+++ src/user/dto/change-password.dto.ts
@@ -5,6 +5,6 @@ export class ChangePasswordDto {
   currentPassword!: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(6, { message: 'La nueva contraseña debe tener al menos 6 caracteres' })
   newPassword!: string;
 }
