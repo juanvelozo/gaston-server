2bb5a0ec6d2072d4a31f91389053b481613d771c
juanvelozo_ <juanvelozomoreno18@gmail.com>
2025-06-25 00:40:49 -0300

    [USER] modifica editar perfil para aceptar archivos para foto de perfil

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Patch, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  Body,
+  UseGuards,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { UserService } from './user.service';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-  constructor(private service: UserService) {}
+  constructor(
+    private service: UserService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
 
   @Get('profile')
   getProfile(@GetUser('sub') userId: number) {
@@ -16,9 +29,27 @@ export class UserController {
   }
 
   @Patch('profile')
-  updateProfile(@GetUser('sub') userId: number, @Body() dto: UpdateProfileDto) {
-    return this.service.updateProfile(userId, dto);
+  @UseInterceptors(FileInterceptor('file'))
+  async updateProfile(
+    @GetUser('sub') userId: number,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() dto: UpdateProfileDto,
+  ) {
+    let imageUrl: string | undefined;
+
+    if (file) {
+      const uploaded = await this.cloudinaryService.uploadImage(file);
+      imageUrl = uploaded.secure_url;
+    }
+
+    const updatedDto = {
+      ...dto,
+      ...(imageUrl && { profileImage: imageUrl }),
+    };
+
+    return this.service.updateProfile(userId, updatedDto);
   }
+
   @Patch('change-password')
   changePassword(@GetUser('sub') userId: number, @Body() dto: ChangePasswordDto) {
     return this.service.changePassword(userId, dto);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), CloudinaryModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, CloudinaryService],
   exports: [UserService],
 })
 export class UserModule {}
